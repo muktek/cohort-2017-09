d9a879d522e1d03893611e124a935cd75534219f
Yair <rogues@es6.today>
2017-09-22 16:02:35 -0500

    Fix properties.

--- src/data/days/day-05.js
+++ src/data/days/day-05.js
@@ -12,10 +12,10 @@ export default {
     html: `
     <ul>
       <li>
-        <p><code>display: flex;</code> is for the parent element.</p>
+        <p><code>display: flex;</code>, <code>justify-content: ...</code> are for the parent element.</p>
       </li>
       <li>
-        <p><code>justify-content: ...</code> and <code>order: ...</code> is for the children elements.</p>
+        <p><code>order: ...</code>, <code>flex: ...</code> is for the children elements.</p>
       </li>
       <li>
         <p><strong>Extra reading: </strong><a href='https://css-tricks.com/snippets/css/a-guide-to-flexbox/'>A Complete Guide to Flexbox | CSS-Tricks</a></p>
